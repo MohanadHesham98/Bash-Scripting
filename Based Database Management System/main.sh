cc6622c95158a2151c00e816f0f4dc23e81d1905
MohanadHesham98 <143830384+MohanadHesham98@users.noreply.github.com>
2025-04-19 20:35:23 +0200

    main.sh

--- Based Database Management System/main.sh
+++ Based Database Management System/main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-
+echo "Main Menu:" 
 source "$SCRIPT_DIR/DB.sh"    # or . ./create_db.sh (dot works the same)
 DB
 source "$SCRIPT_DIR/inside_DB.sh"
